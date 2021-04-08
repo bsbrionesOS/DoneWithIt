b58d1fec80b3117556a3ece73160c9cf28ddccaa
brandon <bsbrionesos@gmail.com>
2021-04-08 13:47:11 -0500

    created the listing detail component

--- App.js
+++ App.js
@@ -5,23 +5,10 @@ import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "gray",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red Jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen></ListingDetailsScreen>;
   // <WelcomeScreen />;
   //<ViewImageScreen />;
 }
