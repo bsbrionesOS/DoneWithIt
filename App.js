9181e125cc427651d51607b6ad7ddad8eb5c7c1c
brandon <bsbrionesos@gmail.com>
2021-04-08 13:36:02 -0500

    created card component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "gray",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
+  // <WelcomeScreen />;
   //<ViewImageScreen />;
 }
