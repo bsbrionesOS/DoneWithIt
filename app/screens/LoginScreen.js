c558b2b6637cd7d1bbb9f5de4cf9f4cca5bbf185
brandon <bsbrionesos@gmail.com>
2021-04-09 04:00:20 -0500

    massive form refactor

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { StyleSheet, Image } from "react-native";
-import { Formik } from "formik";
 import * as Yup from "yup";
 
-import AppTextInput from "../components/AppTextInput";
-import ErrorMessage from "../components/ErrorMessage";
 import Screen from "../components/Screen";
-import SubmitButton from "../components/SubmitButton";
-import AppFormField from "../components/AppFormField";
-import AppForm from "../components/AppForm";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
