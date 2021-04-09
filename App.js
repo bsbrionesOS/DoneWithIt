4d353918011c75046e9e891eddab0bc677eba707
brandon <bsbrionesos@gmail.com>
2021-04-09 04:58:09 -0500

    included dynamic width

--- App.js
+++ App.js
@@ -18,5 +18,5 @@ import LoginScreen from "./app/screens/LoginScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <Messages />;
+  return <ListingEditScreen />;
 }
