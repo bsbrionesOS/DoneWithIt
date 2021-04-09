68864b28291905adeac69f6c77db7a07df68ed07
brandon <bsbrionesos@gmail.com>
2021-04-09 04:23:17 -0500

    fixed listingEditScreen input color

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -30,9 +30,11 @@ function AppPicker({ icon, items, placeholder, onSelectItem, selectedItem }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -76,6 +78,11 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   text: {
+    flex: 1,
+  },
+
+  placeholder: {
+    color: defaultStyles.colors.medium,
     flex: 1,
   },
 });
