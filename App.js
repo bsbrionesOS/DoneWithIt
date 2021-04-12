3c1caa97bc6166b99a535543e8e4b56aa22d4efc
brandon <bsbrionesos@gmail.com>
2021-04-12 17:10:39 -0500

    building image select component

--- App.js
+++ App.js
@@ -9,7 +9,25 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Messages from "./app/screens/MessagesScreen";
+import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/forms/ImageInputList";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
+  return (
+    <ImageInputList
+      imageUris={imageUris}
+      onAddImage={handleAdd}
+      onRemoveImage={handleRemove}
+    />
+  );
 }
