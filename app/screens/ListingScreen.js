f4a0cc8b7258dd6291106ba075276ab0a51b4b45
brandon <bsbrionesos@gmail.com>
2021-04-16 03:40:28 -0500

    created a reusable api call hook

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,33 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, FlatList, ActivityIndicator } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 
+// import ActivityIndicator from "../components/ActivityIndicator";
 import Button from "../components/Button";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-import listingApi from "../api/listings";
+import listingsApi from "../api/listings";
 import AppText from "../components/Text";
+import useApi from "../hooks/useApi";
 
 function ListingScreen({ navigation }) {
-  const [listings, setListings] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { data: listings, error, loading, request: loadListings } = useApi(
+    listingsApi.getListings
+  );
 
   useEffect(() => {
     loadListings();
   }, []);
 
-  const loadListings = async () => {
-    setLoading(true);
-    const response = await listingApi.getListings();
-    setLoading(false);
-
-    if (!response.ok) return setError(true);
-
-    setError(false);
-    setListings(response.data);
-  };
   return (
     <Screen style={styles.screen}>
       {error && (
@@ -36,7 +28,7 @@ function ListingScreen({ navigation }) {
           <Button title="Retry" onPress={loadListings} />
         </>
       )}
-      <ActivityIndicator animating={false} />
+      {/* <ActivityIndicator visible={loading} /> */}
       <FlatList
         data={listings}
         keyExtractor={(listings) => listings.id.toString()}
