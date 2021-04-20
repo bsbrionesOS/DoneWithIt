617c3914f6ea1604a58c18e6c49675d95397c925
brandon <bsbrionesos@gmail.com>
2021-04-19 19:19:49 -0500

    implemented image caching

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import { Image } from "react-native-expo-image-cache";
 import AppText from "../components/Text";
 import colors from "../config/colors";
 
@@ -10,7 +12,12 @@ function ListingDetailsScreen({ route }) {
 
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      <Image
+        style={styles.image}
+        tint="light"
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        uri={listing.images[0].url}
+      />
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>{listing.title}</AppText>
         <AppText style={styles.price}>{listing.price}</AppText>
